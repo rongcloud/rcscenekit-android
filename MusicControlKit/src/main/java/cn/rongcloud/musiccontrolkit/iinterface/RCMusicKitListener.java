c0cc08dd10711240be14c02406a9d5ab43b54088
guoyaning <guoyaning@rongcloud.cn>
2022-01-10 16:43:33 +0800

    feat: 拆分kit库为submodule方式集成

--- MusicControlKit/src/main/java/cn/rongcloud/musiccontrolkit/iinterface/RCMusicKitListener.java
+++
@@ -1,8 +0,0 @@
-package cn.rongcloud.musiccontrolkit.iinterface;
-
-/**
- * @author gyn
- * @date 2021/12/14
- */
-public interface RCMusicKitListener extends OnMusicDataSourceListener, OnMusicOperateListener, OnMusicPlayerListener {
-}
